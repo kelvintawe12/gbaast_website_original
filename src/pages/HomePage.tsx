298414e26d7d1343c8018fca5df94d9fe4471f70
kelvintawe12 <k.tawe@alustudent.com>
2026-03-03 23:13:08 +0200

    feat: Add geographic and contact meta tags in index.html; update sitemap.xml with new campus tours and application process URLs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,7 +24,7 @@ export function HomePage() {
       <AcademicPrograms />
       <StatsSection />
       <NewsEventsSection />
-      <CampusLifeSection />
+      <CampusLicfeSection />
       <ResearchSection />
       <AdmissionsSection />
       <TestimonialsSection />
